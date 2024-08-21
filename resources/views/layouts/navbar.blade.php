5d476102b3bfff39b612e8d34bcf76a06f1754b0
ihanksupremacy <raihan_muhaimin@yahoo.com>
2024-08-21 09:11:29 +0700

    config navbar bisa di klik dipage lain

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -109,3 +109,7 @@
     </ul>
 
 </nav>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap Bundle with Popper -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
